d59eca83b7f5eea98608dc20c5749320628f2a96
Qinglong Zeng <vincentzengcn@outlook.com>
2018-08-14 18:23:09 +0800

    add tf data preprocessing

---
+++ dxtype/model_n_graph.py
@@ -0,0 +1,18 @@
+from dxl.learn.core import Graph, Model
+from dxl.learn.model.cnn import UpSampling2D, Conv2D
+from dxl.learn.model.crop import boundary_crop, align_crop, shape_as_list
+from dxl.learn.model.cnn import UpSampling2D, Conv2D
+
+class SRKeys:
+    pass
+
+
+class SRTrys(Model):
+
+    # TODO
+    class
+
+    pass
+
+g = SRTrys('g')
+g.make()
