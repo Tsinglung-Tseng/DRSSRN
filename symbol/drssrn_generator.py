08deb35ae31f610a8f249ae6b10aa64177e5d5c8
tsinglung <tsinglung.tseng@gmail.com>
2018-08-20 23:15:13 +0800

    add DownSampler

--- symbol/drssrn_generator.py
+++ symbol/drssrn_generator.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 import itertools
 import tables
+import numpy as np
+from singledispatch import singledispatch
+import matplotlib.pyplot as plt
 
 DEFAULT_FILE = '/home/qinglong/node3share/analytical_phantom_sinogram.h5'
 file = tables.open_file(DEFAULT_FILE)
+
 
 
 class DataGen:
@@ -12,10 +16,11 @@ class DataGen:
     g = DataGen(file)
 
     with tf.Session() as sess:
-        sess.run(g.next_)
+        sess.run(g.next_batch)
     """
     def __init__(self, file, batch_size=32):
-        self.file = file
+        self._file = file
+        self.batch_size = batch_size
         self.next_one = (
             tf.data.Dataset
             .from_generator(self._gen,
@@ -38,21 +43,22 @@ class DataGen:
     def _gen(self):
         for i in itertools.count(1):
             # TODO uncoupling path
-            yield self.file.root.data[i][0], self.file.root.data[i][1], self.file.root.data[i][2]
+            yield self._file.root.data[i][0], self._file.root.data[i][1], self._file.root.data[i][2]
 
 
 class DataIte:
+    """
+    i = DataIte
+    """
     def __init__(self):
         ds = tf.data.Dataset.range(25,300)
         ds = ds.shuffle(buffer_size=10)
-        ds = ds.batch(BATCH_SIZE)
-
+        ds = ds.batch(32) #batch_size=32
 
         a_iterator = tf.data.Iterator.from_structure(tf.int64, tf.TensorShape([]))
         prediction = a_iterator.get_next()
 
         ds_iterator = a_iterator.make_initializer(ds)
-
 
         with tf.Session() as sess:
             sess.run(ds_iterator)
@@ -61,6 +67,100 @@ class DataIte:
                     print(sess.run(prediction))
                 except tf.errors.OutOfRangeError:
                     break
+
+
+class DownSampler:
+    """
+    d = DownSampler(input, down_sample_ratios)
+    with tf.Session() as sess:
+        sess.run(d()
+    """
+    def __init__(self, input, down_sample_ratios, batch_size):
+        self.input = input
+        self.down_sample_ratios = down_sample_ratios
+        self.batch_size = batch_size
+        self.input_dim = len(self.shape_as_list(input))
+        self.output_size = self._output_size()
+
+    # @classmethod
+    # def shape_as_list(cls, input):
+    #     if isinstance(input, tf.Tensor):
+    #         return input.shape.as_list()
+
+    @staticmethod
+    def shape_as_list(input):
+        if isinstance(input, tf.Tensor):
+            return list(input.shape.as_list())
+
+    def _output_size(self):
+        if (self.input_dim != 2) and (self.input_dim != 3):
+            raise ValueError("Unsupported tensor dimension: {}, only 2D or 3D tensors are accepted.".format(self.input_dim))
+        else:
+            if self.input_dim == 2:
+                return self._get_down_sample_img_size(self.shape_as_list(self.input))
+                # return list(np.ceil(np.divide(self.shape_as_list(self.input), down_sample_ratios)).astype(np.int32))
+            else:
+                tensor_dim = self.shape_as_list(self.input)
+                # print(tensor_dim)
+                img_dim = self._get_down_sample_img_size(tensor_dim[1:])
+                img_dim.insert(0, self.batch_size)
+                img_dim.append(1)
+                # print('****************************_output_size****************************')
+                # print(img_dim)
+                return img_dim
+
+    def _get_down_sample_img_size(self, origin_dim):
+        return list(np.ceil(np.divide(origin_dim, self.down_sample_ratios)).astype(np.int32))
+
+    def _shape_plus(self):
+        if (self.input_dim != 2) and (self.input_dim != 3):
+            raise ValueError("Unsupported tensor dimension: {}, only 2D or 3D tensors are accepted.".format(self.input_dim))
+        else:
+            if self.input_dim == 2:
+                shape_plus = self.shape_as_list(self.input)
+                shape_plus.append(1)
+                return shape_plus
+            else:
+                shape_plus = self.shape_as_list(self.input)
+                shape_plus[0] = self.batch_size
+                shape_plus.append(1)
+                return shape_plus
+
+    def __call__(self):
+        # shape_plus = self.shape_as_list(self.input)
+        # shape_plus[0] = self.batch_size
+        shape_plus = self._shape_plus()
+        # shape_plus.append(1)
+        # print('****************************__call__****************************')
+        # print(shape_plus)
+        input_reshaped = tf.reshape(self.input, shape_plus)
+        if self.input_dim == 2:
+            return tf.image.resize_images(input_reshaped, tf.convert_to_tensor(self.output_size, dtype=tf.int32))
+        else:
+            return tf.image.resize_images(input_reshaped, tf.convert_to_tensor(self.output_size[1:3], dtype=tf.int32))
+
+
+if __name__ == '__main__':
+    # print('****************************__main__****************************')
+    g = DataGen(file)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        a,b,c = g.next_batch
+
+        # DownSampler.shape_as_list(a)
+        d = DownSampler(a, 8, g.batch_size)
+
+    with tf.Session() as sess:
+        img_down_sample_x2 = sess.run(d())
+
+    # plt.imshow(img_down_sample_x2.reshape(d.output_size))
+    # plt.colorbar()
+    # plt.show()
+
+    img = img_down_sample_x2[1]
+    plt.imshow(img.reshape(d.output_size[1:3]))
+    plt.colorbar()
+    plt.show()
 
 # class gen:
 #     """
@@ -101,5 +201,3 @@ class DataIte:
 #     def _img_gen(self):
 #         for i in itertools.count(1):
 #             yield self.file.root.data[i][0]
-
-
